f987f568526a8fc0e3cc78042aae6663811f44be
Adriani Delfino <141968057+Adri210@users.noreply.github.com>
2025-08-19 22:12:12 -0300

    commit

---
+++ android/app/src/main/kotlin/com/example/braga_resolve/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.braga_resolve
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:braga_resolve/ClienteScreen.dart';
+import 'package:braga_resolve/prestadorscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '',
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TOPO COM IMAGEM NA APPBAR
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: SizedBox(
+          width: double.infinity,
+          child: Image.asset(
+            "assets/images/logo_top.png", // imagem topo
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+
+      // CONTEÚDO PRINCIPAL
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 120,
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'Bem Vindo!',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Conectando prestadores de serviços com clientes de forma rápida e eficiente.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // Botão 1 - Amarelo
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Prestadorscreen()),
+  );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFF9DC06),
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 0,
+                  ),
+                  child: const Text(
+                    'Sou prestador',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              // Botão 2 - Cinza claro
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ClienteScreen()),
+  );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFE5E7EB),
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 0,
+                  ),
+                  child: const Text(
+                    "Sou cliente",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      // FOOTER COM IMAGEM
+      bottomNavigationBar: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: Image.asset(
+          "assets/images/footer.png", // imagem rodapé
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
